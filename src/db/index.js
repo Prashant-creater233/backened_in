9a28f67544351d1b955aba8d68d3d34f884cfd13
Prashant-creater233 <prashantkanwal8@gmail.com>
2026-08-17 22:03:52 +0530

    connect to mongo atlast

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+// second method (this method is good isme yha se export kr denge index.js me import fir wha code jyada nhi hoga)
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance}`);
+    } catch (error) {
+        console.log("MONGODB connection error", error);
+        process.exit(1) 
+    }
+}
+
+export default connectDB
